f3a00965f1961d474f686bd1c5761b911451f0e0
Srihari Mittapally <sriharimittapally95@gmail.com>
2024-11-07 17:50:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+
+const  Blog = require("./models/blog")
+
+const userRoutes = require("./routes/user");
+const blogRoutes = require("./routes/blog");
+
+
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+
+const app=express();
+const PORT = process.env.PORT || 8000;
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(e => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+
+    });
+});
+
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+
+app.listen(PORT,() => {
+   console.log( `Server Started at PORT: ${PORT}`)
+});
